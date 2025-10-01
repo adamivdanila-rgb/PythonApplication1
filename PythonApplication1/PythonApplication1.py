44995949b1ee081f7c4f19529ef737b220fc1b90
opilane <opilane@HP-CZC2349HTS>
2025-10-01 11:08:30 +0300

    add coment #

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,29 @@
 
+# 2.
+#Mis tüüpi on järgnevad muutujad:
+#a) vanus = 18
+#b) eesnimi = "Jaak"
+#c) pikkus = 16.5
+#d) kas_käib_koolis = True
+#Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende muutujate väärtusi koodis kontrollida?
+#Kirjuta kood tüüpide kontrollimiseks.
+vanus = 18        #int
+eesnimi = "Jaak"  #str
+pikkus = 1.65     #float
+kas_käib_koolis = True #bool
+print(f"vanus (vanus) on: {type(vanus)}")
+print(f"eesnimi {eesnimi} on:{type(eesnimi)} ")
+print(f"pikkus {pikkus} on: {type(pikkus)}")
+print(f"kas_käib_koolis {kas_käib_koolis} on: {type(kas_käib_koolis)}
+
+#Harjutus 1.1. Muutujad ja sisend
+#1.
+#Küsi kasutaja nimi ja muuda tekst, et ta näeks välja nii: “Tere, maailm! Tervitan sind Mati”, kui kasutaja nimi on Mati.
+#“Tere, maailm! Tervitan sind Mati! Sa oled N aastat vana.”
+
+print("Tere maailm!")
+nimi=input("Sisesta oma nimi: ")capitalize()#sisend ja ootab enterit
+print(f"Tere maailm! Tervitan sind {nimi}")
+vanus=int(input("Sisesta oma vanus: "))#int teisendab stringi
+print(f"Tere maailm! Tervitan sind {nimi.upper()}. Sa oled {vanus} aastat vana") #upper teeb suurused
+print(f"Tere maailm! Tervitan sind {nimi.upper()}. Sa oled {vanus} aastat vana") #lower 
