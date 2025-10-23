be6d9ff35c1677f0b92a510bc11708deb5c8510c
opilane <opilane@HP-CZC2349HT9>
2025-10-23 11:35:36 +0300

    Töö 3.1

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,55 +1,60 @@
-# print = ("Tere! Olen sinu uus sõber - Python!")
-# nimi =input("Sisesta oma nimi: ")
-# print = (f"{nimi}, oi kui ilus nimi! ")
-# try:
-#     soov=int(input(f"{nimi}! Kas leian Sinu keha indeksi? 0-ei, 1-jah =>")) 
-#     if soov==1:
-#         print = "Indeksi leidmine"
-#         # Küsi pikkust kuni on õiges formaadis ja vahemikus 0-200
-#         while True:
-#             try:
-#                 pikkus = int(input("Mis on sinu pikkus (cm)? "))    
-#                 if 0 < pikkus <= 250:
-#                     break
-#                 else:
-#                     print("Pikkus peab olema vahemikus 0 kuni 250 cm!")
-#             except:
-#                 print("Vale pikkus formaat! Palun sisesta täisvara.")
+
+# Töö 3.1
+#1. Sisesta, mitu neist on täisarvud.
+##k=0 # loendur
+for i in range(15):
+    while True:
+        try:
+            arv=float(input(f"Sisesta {i+1}. arv: "))
+            break
+        except:
+            print("Vale andmetüüp!")
+
+    if int(arv)==arv:
+        print(f"{arv} on täisarv")
+        K = 0  # defineeri muutuja enne kasutamist
+arv = float(input("Sisesta 1. arv: "))
+if arv == int(arv):
+    print(f"{arv} on täisarv")
+    K += 1
+print(f"Täisarve oli kokku {k} tk")
+#2. Küsi kasutajalt arv A ja
+#leia kõigi naturaalarvude summa vahemikus 1 kuni A.
+s=0 #Summa
+while True:
+    try:
+        A=int(input(f"Sisesta A: "))
+        break
+    except:
+        print("Vale andmetüüp!")
+for i in range(1,A+1):
+    s=s+i
+print(f"Summa vahemikus 1 kuni {A} võrdub {s}")
+
+#3. Sisestatakse 8 arvu.
+#Leida nende korrutis (ainult positiivsete arvude puhul).
+korrutis=1
+for i in range(8):
+    while True:
+        try:
+            arv=float(input(f"Sisesta {i+1}. arv: "))
+            if arv<0: break
+        except:
+            print("Vale andmetüüp!")
+    korrutis*=arv
+print(f"Korrutis võrdub {korrutis}")
 
 
-#         # Küsi massi kuni on õiges fotmaadis ja vahemikus 0-200
-#         while True:
-#             try:
-#                 mass = int(input("Mis on sinu kaal (kg)?"))    
-#                 if 0 < mass <= 200:
-#                     break
-#                 else:
-#                     print("Kaal peab olema vahemikus 0 kuni 200 kg!")
-#             except:
-#                 print("Vale kaalu formaat! Palun sisesta arv.")
+#4. Koosta proframm
+#mis väljastab ekraanile arvude ruudud vahemikus 10 kuni 20.
 
-#         # Arvuta kehamassiindeks (KMI)
-#         indeks = round(mass / (0.01 * pikkus) ** 2, 2)
-#         print = (f"{nimi}! Sinu kehamassiindeks on: {indeks}")
-#         # Hinda KMI. Tee seda ise
-#         def arvitu_kmi():
-#             print("KMI hinnang:")
-#             if indeks < 16-19:
-#                 print("Alakaal")
-#             elif 20 <= indeks < 25:
-#                 print("Normaalkaal")
-#             elif 26 <= indeks < 30:
-#                 print("Ülekaal")
-#             elif 31 <= indeks < 35:
-#                 print("Rasvumine")
-#             elif 36 <= indeks < 40:
-#                 print("Tugev rasvumine")
-#             elif indeks >= 40:
-#                 print("Tervisele ohtlik rasvumine")
+for i in range(10,21):
+    print(f"Arv {i} ruut on {i**2}")
 
-#     elif soov==0:
-#         print("Kahju! See on väga kasulik info!")
-#     else:
-#         print("Vale valik. Saab valida ainult 1 või 0")
-# except:
-#     print = "Vale soov!"rt
+#5. Koosta programm
+#mis arvutab ainult negatiivsete arvude summa N sisestatud arvu seast.
+#N väärtus sisestatakse klaviatuurilt.
+#Isaseisvalt
+
+
+ 
